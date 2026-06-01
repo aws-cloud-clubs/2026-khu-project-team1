cb96ac0e690b2d2f06c4008ce3f1260bb703bf3a
kwang-i-coder <wkk021121@gmail.com>
2026-06-01 14:20:52 +0900

    fix: DemoApplcication bean register & 환경변수 설정

--- backend/src/main/java/webhook_inspector/DemoApplication.java
+++ backend/src/main/java/webhook_inspector/DemoApplication.java
@@ -2,12 +2,15 @@ package webhook_inspector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import webhook_inspector.config.AppProperties;
+import webhook_inspector.config.AwsProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({AwsProperties.class, AppProperties.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
